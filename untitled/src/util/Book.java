793b0e238197628488116e8d6d4a0adf306d9c99
PavelSuchkov <you@example.com>
2017-12-16 00:19:53 +0300

    constructors without this

---
+++ untitled/src/util/Book.java
@@ -0,0 +1,22 @@
+package util;
+
+public class Book {
+    private String title;
+    private String publisher;
+    private int price;
+    private String language;
+
+    public Book(String title, String publisher, int price, String language) {
+    }
+
+    public Book(String title, String publisher, String language) {
+    }
+
+    public Book(String title, String publisher, int price) {
+    }
+
+    public Book(String title) {
+
+    }
+
+}
